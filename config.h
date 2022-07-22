f71e93e7b3bf6118069bc799233f4e3d581d018d
Crony Akatsuki <crolinuxgamer@gmail.com>
2022-07-22 18:29:31 +0200

    root commit

---
+++ config.h
@@ -0,0 +1,26 @@
+/* user and group to drop privileges to */
+static const char *user  = "crony";
+static const char *group = "crony";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] = "green",       /* after initialization */
+	[INPUT] = "#005577",    /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+	[CAPS] = "red",         /* CapsLock on */
+};
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color6",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
+		{ "color3",       STRING,  &colorname[CAPS] },
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
